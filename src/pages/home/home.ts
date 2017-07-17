1938965189077e7543f5cb82f0b6df71959e42b1
Christopher Betz <scrapdog@gmail.com>
2017-07-17 11:43:39 -0400

    list of items and take photo functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,58 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { GoogleCloudVisionServiceProvider } from '../../providers/google-cloud-vision-service/google-cloud-vision-service';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items: FirebaseListObservable<any[]>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    private camera: Camera, 
+    private vision: GoogleCloudVisionServiceProvider,
+    private db: AngularFireDatabase, 
+    private alert: AlertController) {
+      this.items = db.list('items'); 
+    }
 
+  takePhoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      targetHeight: 500,
+      targetWidth: 500,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.vision.getLabels(imageData).subscribe((result) => {
+        this.saveResults(imageData, result.json().responses);
+      }, err => {
+        this.showAlert(err);
+      });
+    }, err => {
+      this.showAlert(err);
+    });
+  }
+
+  saveResults(imageData, results) {
+    this.items.push({ imageData: imageData, results: results})
+        .then(_ => { })
+        .catch(err => { this.showAlert(err) });
+  }
+
+  showAlert(message) {
+    let alert = this.alert.create({
+      title: 'Error',
+      subTitle: message,
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
